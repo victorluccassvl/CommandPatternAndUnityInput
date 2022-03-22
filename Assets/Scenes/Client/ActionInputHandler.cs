9cef00572982c1a30a191c5d076d9fcde71ba6f5
DESKTOP-RQ4PIPQ\VictorLuccas <victor.luccas.svl@gmail.com>
2022-03-21 21:58:16 -0300

    Created client commands

---
+++ Assets/Scenes/Client/ActionInputHandler.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using LeRatTools;
+
+public class ActionInputHandler : MonoBehaviour, ICommandClient
+{
+    // Command invoker reference
+    private Player player;
+    private Actions actions;
+
+    private void Awake()
+    {
+        actions = new Actions();
+        actions.TestMap1.Enable();
+    }
+
+    private void Update()
+    {
+        SelectPlayer();
+        HandleInputMapChange();
+        GenerateContinuousCommands();
+        UpdateCamera();
+    }
+
+    void GenerateJumpCommand(InputAction.CallbackContext context)
+    {
+        player.AddCommand<Command_Jump>(new Command_Jump());
+    }
+    void GenerateInflateCommand(InputAction.CallbackContext context)
+    {
+        player.AddCommand<Command_Inflate>(new Command_Inflate());
+    }
+    void GenerateSidestepCommand(InputAction.CallbackContext context)
+    {
+        player.AddCommand<Command_Sidestep>(new Command_Sidestep());
+    }
+    void GenerateDeflateCommand(InputAction.CallbackContext context)
+    {
+        player.AddCommand<Command_Deflate>(new Command_Deflate());
+    }
+
+    private void SubscribeToActions()
+    {
+        actions.TestMap1.Jump.performed += GenerateJumpCommand;
+        actions.TestMap2.Sidestep.performed += GenerateSidestepCommand;
+    }
+    private void UnsubscribeToActions()
+    {
+        actions.TestMap1.Jump.performed -= GenerateJumpCommand;
+        actions.TestMap2.Sidestep.performed -= GenerateSidestepCommand;
+    }
+    private void ResubscribeToActions()
+    {
+        UnsubscribeToActions();
+        SubscribeToActions();
+    }
+
+    private void SelectPlayer()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out hit))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    player = hit.transform.GetComponent<Player>();
+                    ResubscribeToActions();
+                }
+            }
+        }
+    }
+
+    private void HandleInputMapChange()
+    {
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            if (actions.TestMap1.enabled)
+            {
+                actions.TestMap1.Disable();
+                actions.TestMap2.Enable();
+            }
+            else
+            {
+                actions.TestMap1.Enable();
+                actions.TestMap2.Disable();
+            }
+        }
+    }
+
+    private void GenerateContinuousCommands()
+    {
+        if (player == null) return;
+
+        Vector2 input;
+
+        if (actions.TestMap1.Move.IsPressed())
+        {
+            input = actions.TestMap1.Move.ReadValue<Vector2>();
+            player.AddCommand<Command_Move>(new Command_Move(new Vector3(input.y, 0f, -input.x)));
+        }
+
+        if (actions.TestMap1.Inflate.IsPressed()) player.AddCommand<Command_Inflate>(new Command_Inflate());
+
+        if (actions.TestMap2.Rotate.IsPressed())
+        {
+            input = actions.TestMap2.Rotate.ReadValue<Vector2>();
+            player.AddCommand<Command_Rotate>(new Command_Rotate(new Vector3(-input.x, 0f, -input.y)));
+        }
+
+        if (actions.TestMap2.Deflate.IsPressed()) player.AddCommand<Command_Deflate>(new Command_Deflate());
+    }
+
+    private void UpdateCamera()
+    {
+        if (player != null)
+        {
+            Camera.main.transform.position = player.transform.position;
+            Camera.main.transform.position += Vector3.up * 5f + Vector3.back * 5f;
+            Camera.main.transform.forward = player.transform.position - Camera.main.transform.position;
+        }
+    }
+}
---
+++ Assets/Scenes/Client/RawInputHandler.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using LeRatTools;
+
+public class RawInputHandler : MonoBehaviour, ICommandClient
+{
+    // Depends on the abstract invoker
+    // Creates concrete commands
+    // Pass concrete commands to abstract invokers
+
+    public enum InputMap
+    {
+        FirstMap,
+        SecondMap
+    }
+    [SerializeField] private InputMap map = InputMap.FirstMap;
+
+    // Command invoker reference
+    private Player player;
+
+    private void Update()
+    {
+        SelectPlayer();
+        HandleInputMapChange();
+        HandleCommandGeneration();
+        UpdateCamera();
+    }
+
+    private void SelectPlayer()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out hit))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    player = hit.transform.GetComponent<Player>();
+                }
+            }
+        }
+    }
+
+    private void HandleInputMapChange()
+    {
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            map = (map == InputMap.FirstMap) ? map = InputMap.SecondMap : map = InputMap.FirstMap;
+        }
+    }
+
+    private void HandleCommandGeneration()
+    {
+        if (player == null) return;
+
+        Vector2 directional = Vector2.zero;
+        directional.y += Keyboard.current.wKey.ReadValue();
+        directional.y -= Keyboard.current.sKey.ReadValue();
+        directional.x += Keyboard.current.dKey.ReadValue();
+        directional.x -= Keyboard.current.aKey.ReadValue();
+        bool specialKeyPressed = Keyboard.current.fKey.isPressed;
+        bool spaceKeyPressed = Keyboard.current.spaceKey.wasPressedThisFrame;
+
+        switch (map)
+        {
+            case InputMap.FirstMap:
+                if (directional != Vector2.zero) player.AddCommand<Command_Move>(new Command_Move(new Vector3(directional.x, 0f, directional.y)));
+                if (specialKeyPressed) player.AddCommand<Command_Inflate>(new Command_Inflate());
+                if (spaceKeyPressed) player.AddCommand<Command_Jump>(new Command_Jump());
+                break;
+            case InputMap.SecondMap:
+                if (directional != Vector2.zero) player.AddCommand<Command_Rotate>(new Command_Rotate(new Vector3(directional.y, 0f, -directional.x)));
+                if (specialKeyPressed) player.AddCommand<Command_Deflate>(new Command_Deflate());
+                if (spaceKeyPressed) player.AddCommand<Command_Sidestep>(new Command_Sidestep());
+                break;
+        }
+    }
+
+    private void UpdateCamera()
+    {
+        if (player != null)
+        {
+            Camera.main.transform.position = player.transform.position;
+            Camera.main.transform.position += Vector3.up * 5f + Vector3.back * 5f;
+            Camera.main.transform.forward = player.transform.position - Camera.main.transform.position;
+        }
+    }
+}
